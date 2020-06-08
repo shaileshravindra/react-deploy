7990e77b9b5e1caf80a1b74520bde38431f00d8f
Shailesh Ravindra <ravindra.shailesh@gmail.com>
2020-06-08 17:52:47 +0530

    Updates

---
+++ precache-manifest.4a6ef8ec2103407fc8cc8687292216ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ec94026819521f4a6b8f35b4863c32",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "58c653e88acb39b3f737",
+    "url": "/react-deploy/static/css/main.58218f82.chunk.css"
+  },
+  {
+    "revision": "3b9aa6a9857f2d930289",
+    "url": "/react-deploy/static/js/2.9e3ba486.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-deploy/static/js/2.9e3ba486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58c653e88acb39b3f737",
+    "url": "/react-deploy/static/js/main.a90b97a7.chunk.js"
+  },
+  {
+    "revision": "be95eb392b102b3d745a",
+    "url": "/react-deploy/static/js/runtime-main.4396c54d.js"
+  }
+]);
